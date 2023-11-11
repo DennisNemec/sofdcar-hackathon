37e0c31405fe87c0fd094149c8bfa5b65ad68261
Grégoire Mercier <ubnmv@student.kit.edu>
2023-11-11 10:57:59 +0100

    Added Model for vehicle

---
+++ publisher/vehicle.py
@@ -0,0 +1,61 @@
+
+
+def set_gear(g):
+   value = [
+     {
+         "field": "target",
+         "signal": "Vehicle.Powertrain.Transmission.SelectedGear",
+         "value": g,
+         "delay": "0",
+      },
+   ]
+
+def set_break(b):
+   value = [
+      {
+         "field": "target",
+         "signal": "Vehicle.Teleoperation.Brake",
+         "value": b,
+         "delay": "0",
+      }
+   ]
+   return value
+
+def set_turn(s):
+	value = [
+	   {
+         "field": "target",
+         "signal": "Vehicle.Teleoperation.SteeringAngle",
+         "value": "s",
+         "delay": "0",
+      }
+   ]
+   return value
+
+def set_torque(t) :
+   value = [ 
+      {
+         "field": "target",
+         "signal": "Vehicle.Teleoperation.Torque",
+         "value": t,
+         "delay": "0",
+      }
+   ]
+   return value
+
+
+def heartbeat():
+	value = [
+		{
+        	"field": "target",
+         "signal": "Vehicle.Teleoperation.IsEnabled",
+       	"value": "TRUE",
+        	"delay": "0", 
+      }
+	]
+	return value
+
+def main():
+	
+
+main()
